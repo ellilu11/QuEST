2fa484058a264ecde6a10f01f5b340a349695b14
ELLIOT LU <luelliot@msu.edu>
2020-07-16 01:32:57 -0400

    Commit

--- src/integrator/integrator_hilbert.cpp
+++ src/integrator/integrator_hilbert.cpp
@@ -22,7 +22,9 @@ Integrator::HilbertIntegrator<soltype>::HilbertIntegrator(
                       history->array_.shape()[1]),
       dt(dt), beta(beta), omega(omega), interp_order(interp_order), rotating(rotating),
       history(std::move(history)),
-      interactions(std::move(interactions))
+      interactions(std::move(interactions)),
+      rabi(num_solutions),
+      coeffs(interp_order)
 
 {
    for(int solution = 0; solution < num_solutions; ++solution)
@@ -31,75 +33,23 @@ Integrator::HilbertIntegrator<soltype>::HilbertIntegrator(
 }
 
 template <class soltype>
-void Integrator::Hilbert<soltype>::solve(
+void Integrator::HilbertIntegrator<soltype>::solve(
     const log_level_t log_level)
 {
 
  for(int step = 1; step < time_idx_ubound; ++step) {
     assert(0 <= step && step < time_idx_ubound);
 
-    int niter = 0;
-    y_vec = y_prev;
-  
-    if (step%10000 == 0) cout << step << " ";
+  for(int sol_idx = 0; sol_idx < num_solutions; ++sol_idx) {
+    for(int j = 0; j < interp_order; ++j) {
+      history->array_[sol_idx][step][0][0] +=
+        -iu * ( rabi[sol_idx][step-j] * std:conj( history->array_[sol_idx][step-j][0][1] ) * coeffs[j] - 
+                std::conj( rabi[sol_idx][step-j] ) * history->array_[sol_idx][step-j][0][1] * coeffs[j] );
+      history->array_[sol_idx][step][0][1] +=
+        -iu * ( rabi[sol_idx][step-j] * ( 1.0 - 2.0 * history->array_[sol_idx][step-j][0][0] ) * coeffs[j];
 
-    do {
-      update_rhs(step);
-      update_J(step);
-
-      if (step%1000 == 1){
-          Jfile << step << endl;
-          Jfile << rhs_J << endl << endl;
-            // (Eigen::MatrixXd::Identity(DIM*num_solutions,DIM*num_solutions) - dt*rhs_J) << endl;
-      }
-
-      evaluate(step);
-      niter++;
- } while ( x_vec.norm() > EPS ); //&& niter < NUM_ITERATIONS);
-
-  y_prev = y_vec;
-
-   if (step%10000 == 0) cout << endl;
-  }
-}
-
-/*
-template <class soltype>
-void Integrator::NewtonJacobian<soltype>::solve_step(const int step)
-{
-  assert(0 <= step && step < time_idx_ubound);
-
-  y_vec = y_prev;
-  
-  if (step%1000 == 0) cout << step << " ";
-
-  for(int m = 0; m < NUM_ITERATIONS; ++m) {
-    update_rhs(step);
-    update_J(step);
-    evaluate(step);
+    }
   }
 
-  y_prev = y_vec;
-
-  if (step%1000 == 0) cout << endl;
-}*/
-
-template <class soltype>
-int Integrator::NewtonJacobian<soltype>::coord2idx(int row, int col)
-{
-  assert(row != col);
-  if(col > row) std::swap(row, col);
-
-  return row * (row - 1) / 2 + col;
-}
-
-template <class soltype>
-void Integrator::NewtonJacobian<soltype>::log_percentage_complete(
-    const int step) const
-{
-  if(step % (time_idx_ubound / 10) == 0) {
-    std::cout << "\t" << static_cast<int>(10.0 * step / time_idx_ubound)
-              << std::endl;
-  }
 }
 
