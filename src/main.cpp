5615451f068aadbce259618629e9dd92eae53f82
ELLIOT LU <luelliot@msu.edu>
2020-08-22 22:13:37 -0400

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -106,7 +106,7 @@ int main(int argc, char *argv[])
  
     std::vector<std::shared_ptr<InteractionBase>> interactions{ 
       make_shared<PulseInteraction>(qds, pulse1, interpolation_order, c0, dt, hbar, rotating),
-      selfwise};
+      selfwise} ;
 
     if (interacting)
       interactions.push_back( pairwise );
