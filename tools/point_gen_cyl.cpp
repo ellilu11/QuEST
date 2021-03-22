7dd1c47b6ccf8ab8799ab27f531c639fc474152c
ELLIOT LU <luelliot@msu.edu>
2021-03-21 20:01:27 -0400

    commit

--- tools/point_gen_cyl.cpp
+++ tools/point_gen_cyl.cpp
@@ -63,33 +63,30 @@ bool lt_min(const Qdot &qd, const std::vector<Qdot> &dots, const double mindist)
 int main(int argc, char *argv[])
 {
   const double c0 = 299.792458;
-  const double omega = 2278.9013;
+  const double omega = 4823.67; // 2278.9013;
   const double lambda = 2 * M_PI * c0 / omega;
   const double T1 = 10.0, T2 = 20.0;
-  const double dip = 5.2917721e-4;
+  const double dip = 0.002536; // 5.2917721e-4;
   const double dipx = dip, dipy = 0.0, dipz = 0.0;
   const int num_dots = atoi(argv[1]);
 
   const unsigned seed =
       std::chrono::system_clock::now().time_since_epoch().count();
     
-  // const double r, ph;
+  // const double ds = 0.0040 * lambda;
+  // const double xlen = 0.10 * lambda;
+  // const double ylen = xlen;
+  const double R = 0.10 * lambda;
+  const double zlen = 10 * lambda; // std::max( xlen, 0.015 * num_dots * ds );  
+  const double MINDIST = 0.0030;
 
-  const int order = 4;
-  const double ds = 0.0040 * lambda;
-  const double xlen = 320 * ds;
-  const double ylen = xlen;
-  // const double R = 10 * ds;
-  const double zlen = xlen; // std::max( xlen, 0.015 * num_dots * ds );  
-  const double MINDIST = 0.0037;
-
-  std::cout << "xlen: " << xlen/ds << " ylen: " << ylen/ds << " zlen: " << zlen/ds << std::endl;
-  // std::cout << "rlen: " << R/ds << " zlen: " << zlen/ds << std::endl;
+  // std::cout << "xlen: " << xlen/ds << " ylen: " << ylen/ds << " zlen: " << zlen/ds << std::endl;
+  std::cout << "rlen: " << R/lambda << " zlen: " << zlen/lambda << std::endl;
  
   std::default_random_engine generator(seed);
   std::uniform_real_distribution<double> 
-      // rsqdist(0, R*R), phdist(0, 2*M_PI), 
-      xdist(-xlen/2, xlen/2), ydist(-ylen/2, ylen/2),
+      rsqdist(0, R*R), phdist(0, 2*M_PI), 
+      // xdist(-xlen/2, xlen/2), ydist(-ylen/2, ylen/2),
       zdist(-zlen/2, zlen/2);
   // std::uniform_real_distribution<double> u(0, 1);
   // std::uniform_real_distribution<double> v(0, 1);
@@ -98,8 +95,8 @@ int main(int argc, char *argv[])
   dots.reserve(num_dots);
 
   for(int i = 0; i < num_dots; ++i) {
-    /*double r = sqrt(rsqdist(generator));
-    double phpos= phdist(generator);*/
+    double r = sqrt(rsqdist(generator));
+    double ph = phdist(generator);
 
     /*double th = acos( 2*u(generator) - 1 );
     double ph = 2 * M_PI * v(generator);
@@ -111,27 +108,22 @@ int main(int argc, char *argv[])
     Qdot qd;
 
     while (lt_minflag){
-        //qd = {{r * cos(phpos), r * sin(phpos), zdist(generator), omega,
-        //        T1, T2, dipx, dipy, dipz}};
+        qd = {{r * cos(ph), r * sin(ph), zdist(generator), omega,
+                T1, T2, dipx, dipy, dipz}};
  
-        qd = {{xdist(generator), ydist(generator), zdist(generator), omega,
-                    T1, T2, dipx, dipy, dipz}};
+        // qd = {{xdist(generator), ydist(generator), zdist(generator), omega,
+         //           T1, T2, dipx, dipy, dipz}};
         lt_minflag = lt_min( qd, dots, MINDIST );
     }  
  
     dots.push_back(qd);
   }
 
-
-
-  /*std::sort(dots.begin(), dots.end(), [](const Qdot &a, const Qdot &b) {
-    return a[0] * a[0] + a[1] * a[1] + a[2] * a[2] <
-           b[0] * b[0] + b[1] * b[1] + b[2] * b[2];
-  });*/
-
   min_dist(dots);
 
-  std::ofstream fd("dots.cfg");
+  std::string taskstr(argv[2]);
+
+  std::ofstream fd("./dots/dots"+taskstr+".cfg");
   fd << std::setprecision(12);
 
   for(const auto &d : dots) {
